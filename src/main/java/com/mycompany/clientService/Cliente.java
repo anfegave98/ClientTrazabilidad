813bdf86464730325e780f99e4b970b3065e95bd
brown <brown@BROWN3212.academia.loc>
2019-05-09 12:03:52 -0500

    Integracion

--- src/main/java/com/mycompany/clientService/Cliente.java
+++ src/main/java/com/mycompany/clientService/Cliente.java
@@ -1,9 +1,12 @@
+package com.mycompany.clientService;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+import com.mycompany.model.Paciente;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
